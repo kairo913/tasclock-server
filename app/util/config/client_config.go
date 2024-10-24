839a076c31010ef0176155a3d650704899a3d0c9
kairo913 <148944926+kairo913@users.noreply.github.com>
2024-10-24 10:52:42 +0900

    Add csrf middleware deps

--- app/util/config/client_config.go
+++ app/util/config/client_config.go
@@ -8,7 +8,8 @@ import (
 )
 
 type ClientConfig struct {
-	Port string `env:"CLIENT_PORT, default=3000"`
+	Port       string `env:"CLIENT_PORT, default=3000"`
+	CSRFSecret string `env:"CSRF_SECRET, required"`
 }
 
 func NewClientConfig(ctx context.Context) *ClientConfig {
