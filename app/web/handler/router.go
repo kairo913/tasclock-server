6c53aa875d2f79b4aca4bfaae29afeb6a7498018
kairo913 <148944926+kairo913@users.noreply.github.com>
2024-10-24 10:52:03 +0900

    Add CSRF middleware

--- app/web/handler/router.go
+++ app/web/handler/router.go
@@ -2,10 +2,12 @@ package handler
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kairo913/tasclock-server/app/infra/database"
 	"github.com/kairo913/tasclock-server/app/util/config"
+	csrf "github.com/utrack/gin-csrf"
 )
 
 func SetUpRouter(c context.Context) (*gin.Engine, error) {
@@ -20,6 +22,14 @@ func SetUpRouter(c context.Context) (*gin.Engine, error) {
 	cfg := config.NewClientConfig(c)
 
 	router.Use(CORSMiddleware(cfg.Port))
+
+	router.Use(csrf.Middleware(csrf.Options{
+		Secret: cfg.CSRFSecret,
+		ErrorFunc: func(c *gin.Context) {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "CSRF token mismatch"})
+			c.Abort()
+		},
+	}))
 
 	NewUserHandler(c, router.Group("/user"), sqlHandler)
 
