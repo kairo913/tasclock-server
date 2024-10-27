2383a64d096f40e57495b4e36bde3948f4838cb2
kairo913 <148944926+kairo913@users.noreply.github.com>
2024-10-27 12:21:17 +0900

    Add status endpoint to get csrf token

--- app/web/handler/user_handler.go
+++ app/web/handler/user_handler.go
@@ -2,17 +2,22 @@ package handler
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kairo913/tasclock-server/app/core/service"
 	"github.com/kairo913/tasclock-server/app/infra/database"
 	"github.com/kairo913/tasclock-server/app/infra/repository"
 	"github.com/kairo913/tasclock-server/app/web/controller"
+	csrf "github.com/utrack/gin-csrf"
 )
 
 func NewUserHandler(ctx context.Context, userGroup *gin.RouterGroup, sqlHandler *database.SqlHandler) {
 	userController := controller.NewUserController(service.NewUserAppService(ctx, repository.NewUserRepository(sqlHandler)))
 
+	userGroup.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"token": csrf.GetToken(c)})
+	})
 	userGroup.POST("/signup", userController.SignUp)
 	userGroup.POST("/signin", userController.SignIn)
 }
