2e2a113970e1a4876de6178a729228956a5e02a2
Yagami-Huki <86300861+Yagami-Huki@users.noreply.github.com>
2024-12-11 17:55:47 +0900

    Update user tables

--- docs/user.sql
+++ docs/user.sql
@@ -1,10 +1,16 @@
-CREATE TABLE IF NOT EXISTS user {
+CREATE TABLE IF NOT EXISTS user_info {
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id varchar(36) NOT NULL,
     lastname varchar(255) NOT NULL,
     firstname varchar(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+}
+
+CREATE TABLE IF NOT EXISTS user_auth {
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    uid INTEGER NOT NULL,
     email varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    foreign key (uid) references user(id),
 }
